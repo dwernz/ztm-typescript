bdae28ba1408fe69007aad8d2640040c07ad00f4
Daniel <62781672+dwernz@users.noreply.github.com>
2024-01-08 12:43:15 -0500

    initial commit

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+# Fundamental Data Types
+# int, float, bool, str, list, tuple, set, dict
+
+# Classes -> custom data types
+
+# Specialized Data Types
+# Not built into Python, but special packages and modules.
+
+# None Data Type
+# Nothing, or zero. No value.
+
+# int and float
+print(type(6))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))
+
+# power
+print(2 ** 2)
+print(2 ** 3)
+
+# returns rounded down answer
+print(2 // 4)
+
+# returns remainder
+print(6 % 4)
